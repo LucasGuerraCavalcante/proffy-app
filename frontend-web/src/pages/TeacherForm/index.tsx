a31fd37006869162967040aed914ef0cde5dd819
Lucas Guerra <lucasguerratee@gmail.com>
2020-08-04 01:57:09 -0300

    TeacherForm page

---
+++ frontend-web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Sign up to teach" >
+                A
+            </PageHeader>
+        </div>
+    )
+}
+
+export default TeacherForm;
