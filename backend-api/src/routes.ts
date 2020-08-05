02ceeb8ec01bf841aeb83859d061465322adcd68
Lucas Guerra <lucasguerratee@gmail.com>
2020-08-05 00:48:40 -0300

    classesControler

--- backend-api/src/routes.ts
+++ backend-api/src/routes.ts
@@ -1,69 +1,11 @@
-import express, { text } from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utilis/convertHourToMinutes';
+import express from 'express';
+
+import ClassesControler from './controllers/ClassesControler';
 
 const routes = express.Router();
+const classesControler = new ClassesControler();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
-
-routes.post('/classes', async (request, response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        });
-    
-        const user_id = insertedUsersIds[0];
-    
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id,
-        });
-    
-        const class_id = insertedClassesIds[0];
-    
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHourToMinutes(scheduleItem.from),
-                to: convertHourToMinutes(scheduleItem.to),
-            };
-        });
-    
-        await trx('class_schedule').insert(classSchedule);
-        
-        await trx.commit();
-
-        return response.status(201).send();
-
-    } catch(err) {
-        await trx.rollback();
-
-        console.log(err);
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-    }
-});
+routes.get('/classes', classesControler.index);
+routes.post('/classes', classesControler.create);
 
 export default routes;
