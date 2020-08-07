905d1eda758c226af28ae16ed162332d7a49cd19
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-08-07 18:35:44 -0300

    navigation and rectbutton

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from  'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -9,6 +11,12 @@ import heartIcon from '../../assets/images/icons/heart.png';
 import styles from './styles';
 
 function Landing() {
+    const { navigate } = useNavigation();
+
+    function navigateToGiveClasses() {
+        navigate('GiveClasses');
+    }
+
     return (
         <View style={styles.container} >
             <Image source={landingImg} style={styles.banner} />
@@ -21,15 +29,21 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                
+                    style={[styles.button, styles.buttonPrimary]}
+                >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Study</Text>
-                </TouchableOpacity>
+                </RectButton>
 
-                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                <RectButton 
+                    onPress={navigateToGiveClasses} 
+                    style={[styles.button, styles.buttonSecondary]}
+                >
                     <Image source={giveClassesIcon} />
                     <Text style={styles.buttonText}>Teach</Text>
-                </TouchableOpacity>
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
