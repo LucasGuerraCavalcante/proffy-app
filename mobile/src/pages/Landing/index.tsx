deab39377059d80251cb5acf8dc8ca8c61cc5919
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-08-08 18:38:17 -0300

    api connection

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from  'react';
+import React, { useState, useEffect } from  'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -8,10 +8,22 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import api from '../../services/api';
 import styles from './styles';
 
 function Landing() {
     const { navigate } = useNavigation();
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+            console.log(response.data);
+
+            setTotalConnections(total);
+        })
+    }, []);
 
     function navigateToGiveClasses() {
         navigate('GiveClasses');
@@ -51,7 +63,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                X Connections {' '}
+                {totalConnections} Connections {' '}
                 <Image source={heartIcon} />
             </Text>
 
